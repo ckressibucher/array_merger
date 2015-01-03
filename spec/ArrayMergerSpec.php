cacc4b66b7ae5bba1c637eaf75e54fcfc4cb9cb0
Claudio Kressibucher <ck@inmedias.de>
2015-01-03 23:19:11 +0100

    first implementation

---
+++ spec/ArrayMergerSpec.php
@@ -0,0 +1,189 @@
+<?php
+
+namespace spec\Ckr\Util;
+
+use Ckr\Util\ArrayMerger;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class ArrayMergerSpec extends ObjectBehavior
+{
+
+    function it_is_initializable()
+    {
+        $this->beConstructedWith(array(), array());
+        $this->shouldHaveType('Ckr\Util\ArrayMerger');
+    }
+
+    // ==========================================================
+    // =========== Behaviour with default configuration        ==
+    // ==========================================================
+
+    function it_should_overwrite_string_keys_given_default_configuration()
+    {
+        $default = ['a' => 1];
+        $precedence = ['a' => 2];
+
+        $expected = [
+            'a' => 2,   // from $precedence
+        ];
+        $this->beConstructedWith($default, $precedence);
+        $result = $this->mergeData();
+        $result->shouldBeAnArray();
+        $result->shouldHaveSameData($expected);
+    }
+
+    function it_should_preserve_a_string_key_given_default_configuration()
+    {
+        $default = ['a' => 1];
+        $precedence = ['b' => 2, 0 => 3]; // should not interfere value of key 'a'
+        $expected = ['a' => 1, 'b' => 2, 0 => 3];
+
+        $this->beConstructedWith($default, $precedence);
+        $this->mergeData()->shouldHaveSameData($expected);
+    }
+
+    function it_should_append_a_numeric_key_given_default_configuration()
+    {
+        $default = [1 => 'a'];
+        $precedence = [1 => 'b'];
+        $expected = [1 => 'a', 2 => 'b'];
+
+        $this->beConstructedWith($default, $precedence);
+        $this->mergeData()->shouldHaveSameData($expected);
+    }
+
+    function it_should_work_recursively_given_sub_array_with_same_string_key_and_default_config()
+    {
+        $default = ['outer' => ['a' => 1, 'b' => 2]];
+        $precedence = ['outer' => ['b' => 'new-b', 'c' => 'c']];
+        $expected = [
+            'outer' => ['a' => 1, 'b' => 'new-b', 'c' => 'c']
+        ];
+        $this->beConstructedWith($default, $precedence);
+        $this->mergeData()->shouldHaveSameData($expected);
+    }
+
+    function it_should_throw_an_exception_on_different_dimensions_given_default_config()
+    {
+        $default = ['k' => 'value'];
+        $precedence = ['k' => ['value']];
+
+        $this->beConstructedWith($default, $precedence);
+        $this->shouldThrow('\UnexpectedValueException')->during('mergeData', [$default, $precedence]);
+    }
+
+    // ========================================================================
+    // =========== Behaviour with flag FLAG_ALLOW_SCALAR_TO_ARRAY_CONVERSION ==
+    // ========================================================================
+
+    function it_should_convert_scalar_to_array_in_default_given_flag_is_set()
+    {
+        $default = ['k' => 'value']; // will be converted to ['k' => [0 => 'value']]
+        $precedence = ['k' => ['othervalue']];
+        $flags = ArrayMerger::FLAG_ALLOW_SCALAR_TO_ARRAY_CONVERSION;
+
+        $expected = ['k' => [0 => 'value', 1 => 'othervalue']];
+
+        $this->beConstructedWith($default, $precedence, $flags);
+        $this->mergeData()->shouldHaveSameData($expected);
+    }
+
+    function it_should_convert_scalar_to_array_in_precedence_given_flag_is_set()
+    {
+        $default = ['k' => ['value']];
+        $precedence = ['k' => 'othervalue'];
+        $flags = ArrayMerger::FLAG_ALLOW_SCALAR_TO_ARRAY_CONVERSION;
+
+        $expected = ['k' => [0 => 'value', 1 => 'othervalue']];
+
+        $this->beConstructedWith($default, $precedence, $flags);
+        $this->mergeData()->shouldHaveSameData($expected);
+    }
+
+    // ======================================================================
+    // ========== behaviour with flag FLAG_OVERWRITE_NUMERIC_KEY      =======
+    // ======================================================================
+
+    function it_should_overwrite_numeric_key_given_flag_is_set()
+    {
+        $default = [1 => 'a'];
+        $precedence = [1 => 'b'];
+        $flags = ArrayMerger::FLAG_OVERWRITE_NUMERIC_KEY;
+        $expected = [1 => 'b'];
+
+        $this->beConstructedWith($default, $precedence, $flags);
+        $this->mergeData()->shouldHaveSameData($expected);
+    }
+
+    function it_should_overwrite_numeric_key_given_flag_is_set_and_prevent_doubles_flag_is_also_set()
+    {
+        $default = [1 => 'a', 2 => 'b'];
+        $precedence = [1 => 'b'];
+        $flags = ArrayMerger::FLAG_OVERWRITE_NUMERIC_KEY | ArrayMerger::FLAG_PREVENT_DOUBLE_VALUE_WHEN_APPENDING_NUMERIC_KEYS;
+        $expected = [1 => 'b', 2 => 'b'];
+
+        $this->beConstructedWith($default, $precedence, $flags);
+        $this->mergeData()->shouldHaveSameData($expected);
+    }
+
+
+    // ======================================================================
+    // ========== behaviour with flags FLAG_OVERWRITE_NUMERIC_KEY     =======
+    // ==========    and FLAG_ALLOW_SCALAR_TO_ARRAY_CONVERSION set    =======
+    // ======================================================================
+
+    function it_should_overwrite_numeric_key_after_conversion_if_flags_are_set()
+    {
+        $default = ['k' => 'value'];
+        $precedence = ['k' => [0 => 'othervalue']];
+        $flags = ArrayMerger::FLAG_OVERWRITE_NUMERIC_KEY | ArrayMerger::FLAG_ALLOW_SCALAR_TO_ARRAY_CONVERSION;
+        $expected = ['k' => [0 => 'othervalue']];
+
+        $this->beConstructedWith($default, $precedence, $flags);
+        $this->mergeData()->shouldHaveSameData($expected);
+    }
+
+
+    // ===============================================================================
+    // ========== behaviour with flag FLAG_PREVENT_DOUBLE_VALUE_ON_NUMERIC_KEY =======
+    // ===============================================================================
+
+    function it_should_skip_existent_key_given_flag_is_set()
+    {
+        $default = [0 => 'value', 2 => 'x', 3 => 'y', 4 => 'z'];
+        $precedence = [1 => 'value', 2 => 'y', 4 => 'z', 100 => 'valid'];
+        $flags = ArrayMerger::FLAG_PREVENT_DOUBLE_VALUE_WHEN_APPENDING_NUMERIC_KEYS;
+
+        $expected = $default;
+        $expected[] = 'valid';
+
+        $this->beConstructedWith($default, $precedence, $flags);
+        $this->mergeData()->shouldHaveSameData($expected);
+    }
+
+    public function getMatchers()
+    {
+        return array(
+            // checks that two arrays have the same data (ignoring the order of string keys)
+            'haveSameData' => function($subject, $expectedArray) {
+                foreach ($expectedArray as $key => $valueExpected) {
+                    if (!\array_key_exists($key, $subject)) return false;
+                    if (\is_array($valueExpected)) {
+                        $m = $this->getMatchers();
+                        $fnSameData = $m['haveSameData'];
+                        $res = $fnSameData($subject[$key], $valueExpected);
+                        if (false === $res) return false;
+                    } else {
+                        if ($valueExpected !== $subject[$key]) return false;
+                    }
+                    unset($subject[$key]);
+                }
+                return \count($subject) === 0;
+            },
+            'beAnArray' => function($subject) {
+                return (bool) \is_array($subject);
+            }
+        );
+    }
+}
---
+++ src/ArrayMerger.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace Ckr\Util;
+
+class ArrayMerger
+{
+
+    /**
+     * Given the default array has a scalar 'value' at key 'k' and
+     * the precedence array has a sub array at the same key 'k'.
+     *
+     * If flag is set, the default scalar value 'value' is wrapped in
+     * an array [0 => 'value'] before the merge is done.
+     *
+     * If the flag is NOT set, then an exception is thrown.
+     *
+     * Default: not set
+     */
+    const FLAG_ALLOW_SCALAR_TO_ARRAY_CONVERSION = 1;
+
+    /**
+     * If flag is set, the value of the precedence array for a given key
+     * will overwrite the value of the default array for the same key.
+     *
+     * If flag is NOT set, the value of the precedence array is appended
+     * to the default array. (or skipped, depending on
+     * FLAG_PREVENT_DOUBLE_VALUE_WHEN_APPENDING_NUMERIC_KEYS)
+     *
+     * Default: not set
+     */
+    const FLAG_OVERWRITE_NUMERIC_KEY = 2;
+
+    /**
+     * It flag is set, a value of the precedence array for a numeric key is
+     * only appended, if it does not exist yet in the default array.
+     *
+     * If flag is NOT set, the value of the precedence array is appended
+     * to the default array (or overwrites the default array's value with the same key,
+     * depending on FLAG_ALLOW_SCALAR_TO_ARRAY_CONVERSION)
+     *
+     * This flag has the lower priority as FLAG_OVERWRITE_NUMERIC_KEY, i.e. if
+     * FLAG_OVERWRITE_NUMERIC_KEY is set, then it doesn't matter what the value of
+     * FLAG_PREVENT_DOUBLE_VALUE_WHEN_APPENDING_NUMERIC_KEYS is
+     * (the value is added to the default array at the given index, regardless of other values
+     *  in the array)
+     *
+     * Note that this flag may slow down the operation on very large (default) arrays.
+     */
+    const FLAG_PREVENT_DOUBLE_VALUE_WHEN_APPENDING_NUMERIC_KEYS = 4;
+
+    protected $default;
+
+    protected $precedence;
+
+    protected $flags;
+
+    public function __construct(array $default, array $precedence, $flags = 0)
+    {
+        $this->default = $default;
+        $this->precedence = $precedence;
+        $this->flags = $flags;
+    }
+
+    public function mergeData()
+    {
+        $precedence = $this->precedence;
+        $default = $this->default;
+
+        return static::doMerge($default, $precedence, $this->flags);
+    }
+
+    public static function doMerge(array $default, array $precedence, $flags)
+    {
+        foreach ($precedence as $key => $pVal) {
+            if (\is_numeric($key) && (0 === ($flags & self::FLAG_OVERWRITE_NUMERIC_KEY))) {
+                if (0 === ($flags & self::FLAG_PREVENT_DOUBLE_VALUE_WHEN_APPENDING_NUMERIC_KEYS)
+                    || !\in_array($pVal, $default)
+                ) {
+                    $default[] = $pVal;
+                }
+                continue;
+            }
+            if (\array_key_exists($key, $default)) {
+                $default[$key] = static::mergeRecursively($default[$key], $pVal, $flags);
+            } else {
+                $default[$key] = $pVal;
+            }
+        }
+        return $default;
+    }
+
+    protected static function mergeRecursively($default, $precedence, $flags)
+    {
+        if (\is_array($default) && \is_array($precedence)) {
+            return static::doMerge($default, $precedence, $flags);
+        }
+        if (! \is_array($default) && ! \is_array($precedence)) {
+            return $precedence; // overwrite default by precedence
+        }
+        if (! $flags & self::FLAG_ALLOW_SCALAR_TO_ARRAY_CONVERSION) {
+            throw new \UnexpectedValueException('different dimensions');
+        }
+        if (! \is_array($default)) {
+            $default = array(0 => $default);
+        } else {
+            $precedence = array(0 => $precedence);
+        }
+        return static::doMerge($default, $precedence, $flags);
+    }
+}
